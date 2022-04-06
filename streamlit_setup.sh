0c323de1dbe3ee261c0aeed3175c0a3fa7afd300
paventis <jfeng1669@gmail.com>
2022-04-06 10:32:19 -0400

    added heroku files

--- streamlit_setup.sh
+++ streamlit_setup.sh
@@ -2,9 +2,5 @@ mkdir -p ~/.streamlit
 
 echo "[server]
 headless = true
-port = $PORT
 enableCORS = false
-" > ~/.streamlit/config.toml
-
-echo "password='streamlit_is_great'
 " > ~/.streamlit/config.toml
